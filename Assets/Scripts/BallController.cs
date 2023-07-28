d25dedb6bec8643817032d346247e82f8706a574
Groggula <111517903+Groggula@users.noreply.github.com>
2023-07-28 21:58:07 +0200

    Ball logic added

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
@@ -8,17 +6,41 @@ public class BallController : MonoBehaviour
     public float knockbackForce;
 
     private Rigidbody rb;
-    private GameObject player;
+    private Vector2 initialDirection;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        player = GameObject.Find("PlayerOne");
+        initialDirection = RandomVector(-1f, 1f).normalized;
+        rb.velocity = initialDirection * speed;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        Vector3 direction = (player.transform.position - transform.position).normalized;
-        rb.AddForce(direction * speed);
+        if (transform.position.x > 15 || transform.position.x < -15)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private Vector2 RandomVector(float min, float max)
+    {
+        return new Vector2(Random.Range(min, max), Random.Range(min, max));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 collisionNormal = collision.contacts[0].normal;
+        Vector3 reflectedVelocity = Vector3.Reflect(rb.velocity, collisionNormal);
+        rb.velocity = reflectedVelocity.normalized * speed;
+
+        if (collision.gameObject.name == "PlayerOne")
+        {
+            Debug.Log("Player 1 hit ball");
+        }
+        else if (collision.gameObject.name == "PlayerTwo")
+        {
+            Debug.Log("Player 2 hit ball");
+        }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject ball;
+    public GameObject[] powerup;
+
     void Start()
     {
-        
+        //SpawnBall();
+        InvokeRepeating("SpawnBall", 1, 2);
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnBall()
     {
-        
+        Vector3 spawnPoint = new Vector3(0, 0, 3.7f);
+
+        Instantiate(ball, spawnPoint, Quaternion.identity);
     }
 }
